d0effc7dce379abc3c1c9b6065242818828a6756
Eason Lin <uu9924079@gmail.com>
2022-02-12 15:03:39 +0800

    menu-bar

---
+++ menu-bar/main.js
@@ -0,0 +1,13 @@
+// document.documentElement.style.setProperty('--window-height', window.innerHeight);
+
+const menu = document.querySelector('nav');
+const menuBtn = document.querySelector('button#open');
+menuBtn.addEventListener('click', () => {
+  menu.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+})
+const closeMenuBtn = document.querySelector('button#close');
+closeMenuBtn.addEventListener('click', () => {
+  menu.style.display = 'none';
+  document.body.style.overflow = null;
+})
