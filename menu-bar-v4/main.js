ec6d314a901b378e4004156b272362ac92604281
Eason Lin <eason_lin@addcn.com>
2022-02-18 11:56:05 +0800

    100%

--- menu-bar-v4/main.js
+++ menu-bar-v4/main.js
@@ -6,14 +6,12 @@ let scrollY = 0;
 menuBtn.addEventListener("click", () => {
   scrollY = window.pageYOffset;
   menu.style.display = "block";
-  document.body.classList.add("overhidden");
-  document.documentElement.classList.add("overhidden");
+  document.body.style.overflow = "hidden";
 });
 const closeMenuBtn = document.querySelector("button#close");
 closeMenuBtn.addEventListener("click", () => {
   menu.style.display = "none";
-  document.body.classList.remove("overhidden");
-  document.documentElement.classList.remove("overhidden");
+  document.body.style.overflow = null;
   if (scrollY) {
       window.scrollTo(0, scrollY)
   }
