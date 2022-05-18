5440ca2bd3927d7853663abd8eeffc21905ace37
Eason Lin <uu9924079@gmail.com>
2022-05-18 17:07:32 +0800

    add

--- editor-choice/main.js
+++ editor-choice/main.js
@@ -1,8 +1,9 @@
-axios.get('https://randomuser.me/api/')
-.then(res => {
-    const response = res.data.results[0]
-    const title = document.querySelector('h2');
-    const content = document.querySelector('p');
-    title.textContent = response.firstName + response.lastName
-    content.textContent = response.email
-})
+fetch("https://randomuser.me/api/")
+  .then((nativeData) => nativeData.json())
+  .then((data) => {
+    const result = data.results[0];
+    const title = document.querySelector("h2");
+    const content = document.querySelector("p");
+    title.textContent = result.firstName + " " + result.lastName;
+    content.textContent = result.email;
+  });
